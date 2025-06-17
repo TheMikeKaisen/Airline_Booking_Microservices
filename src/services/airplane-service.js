f399b1127d53dadfa72fd9d63b0329d9ca73fa2b
Apple <h.karthiknair@gmail.com>
2025-06-17 20:05:35 +0530

    add get airplane api

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -37,7 +37,21 @@ async function getAirplanes(){
     }
 }
 
+async function getAirplane(id) {
+    try {
+        const airplane = await airplaneRepository.get(id);
+        return airplane;
+    } catch (error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND){
+            throw new AppError("The airplane you requested couldn't be found!", error.statusCode)
+        }
+        throw new AppError("Cannot fetch data of all the airplanes", StatusCodes.INTERNAL_SERVER_ERROR);
+        
+    }
+}
+
 module.exports = {
     createAirplane,
-    getAirplanes
+    getAirplanes,
+    getAirplane
 }
