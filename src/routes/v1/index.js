75ca62cb1c2db5c11522c22e169bc7d72059af1c
TheMikeKaisen <h.karthiknair@gmail.com>
2025-06-25 23:09:33 +0530

    functionality for creating a User, Add error handlers

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
+const UserController = require('../../controllers/user-controller');
+// const { AuthRequestValidators } = require('../../middlewares/index');
+
 
 const router = express.Router();
 
 router.get('/info', InfoController.info);
+
+router.post(
+    '/signup',
+    UserController.create
+);
 
 module.exports = router;
