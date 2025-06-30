a8955c68b855306d9625c89e88f9efe7e74c83ca
TheMikeKaisen <h.karthiknair@gmail.com>
2025-06-30 21:54:43 +0530

    add nodemailer logic to send email

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { sendBasicEmail } = require('./services/email-service');
 
 const app = express();
 
@@ -12,4 +13,10 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    // sendBasicEmail(
+    //     "themikekaisen@gmail.com", 
+    //     "hie, how are you!",
+    //     "ore wa saikyoo dakara!"
+        
+    // )
 });
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+sendBasicEmail(
+        "themikekaisen@gmail.com", 
+        "hie, how are you!",
+        "ore wa saikyoo dakara!"
+        
+    )
